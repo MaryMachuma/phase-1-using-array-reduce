d06dc1f934df5d7e0c5ccf8c07595131e1763da0
MaryMachuma <machhmary173@gmail.com>
2025-01-13 12:58:29 +0300

    Adds phase-1-using-array-reduce

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+// Use the reduce method to calculate the total number of batteries
+const totalBatteries = batteryBatches.reduce((total, batch) => total + batch, 0);
+module.exports = { totalBatteries };
